523d2e4886141d457262967043cc23a8f9cf4a40
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-28 19:49:42 +0300

    apply betty

--- 0-format_output.c
+++ 0-format_output.c
@@ -7,54 +7,54 @@
 */
 int _printf(const char *format, ...)
 {
-    /* remember betty */
-    va_list args;
-    int count_of_characters;
-    char character;
-    char *string;
+	/* remember betty */
+	va_list args;
+	int count_of_characters;
+	char character;
+	char *string;
 
-    va_start(args, format);/*initializes the va_list*/
+	va_start(args, format);/*initializes the va_list*/
 
-    /*loop through the 3 specifiers c, s, % */
-    /*if characters before NULL, print their count*/
+	/*loop through the 3 specifiers c, s, % */
+	/*if characters before NULL, print their count*/
 
-    /* loop through format and *format to ensure they are not null*/
-    while (*format && format)
-    {    
-        if (format != '%')
-            {
-                /*checks if each char in the !=% and prints*/
-                _putchar(format[count_of_characters]);
-                count_of_characters++;
-            }
-        else
-        {
-            format++;
-            character = *format;
-            switch (character)
-            {
-            case 'c':
-                _putchar(va_arg(args, int));
-                count_of_characters++;
-                break;
-            case 's':
-                string = va_arg(args, char *);
-                count_of_characters++;
-                break;
-            case '%':
-                _putchar('%');
-                count_of_characters++;
-                break;
+	/* loop through format and *format to ensure they are not null*/
+	while (*format && format)
+	{    
+		if (format != '%')
+			{
+			/*checks if each char in the !=% and prints*/
+			_putchar(format[count_of_characters]);
+			count_of_characters++;
+			}
+		else
+		{
+			format++;
+			character = *format;
+			switch (character)
+		{
+			case 'c':
+			_putchar(va_arg(args, int));
+				count_of_characters++;
+				break;
+			case 's':
+				string = va_arg(args, char *);
+				count_of_characters++;
+				break;
+			case '%':
+				_putchar('%');
+				count_of_characters++;
+				break;
             
-            default:
-                _putchar('%');
-                _putchar(character);
-                count_of_characters += 2;
-                break;
-            }
-        }
-    }
-    va_end(args);
+			default:
+				_putchar('%');
+				_putchar(character);
+				count_of_characters += 2;
+				break;
+			}
+		}
+	}
+	va_end(args);
 
-    return (count_of_characters);
+	return (count_of_characters);
 }
