40f859b479742a5cdba94340b87e8eb5f0465d9e
TKaburu <traceymwendwa@gmail.com>
2023-05-28 18:31:56 +0300

    This function will help print 's'

---
+++ puts-string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * puts_str - This function helps prints out  string
+ * @string: This is the string to be printed
+ *
+ * Return: the string
+ */
+
+int puts_str(char *string)
+{
+
+	int index = 0, val = 0;
+
+	if (index)
+	{
+		for (; string[index] != '\0'; index++)
+		{
+			_putchar(string[index]);
+			val += 1;
+		}
+		_putchar('\n);
+		retun (val)
+	}
+}
