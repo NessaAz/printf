ed88c9347b890070797439ed855e53b863f0e9ba
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-05-28 20:18:26 +0300

    refactor

--- 1-format_output.c
+++ 1-format_output.c
@@ -1,13 +1,17 @@
 #include "main.h"
 
-
+/**
+ * _printf - produces output according to a format
+ * @format: is a character string
+ * Return: number of characters printed - count_of_characters
+*/
 
 int _printf(const char *format, ...)
 {
-	va_list arg;
+	va_list args;
 	unsigned value, t; /* value is the return value*/
 
-	va_start(arg, format);
+	va_start(args, format);
 
 	t = 0;
 
@@ -24,22 +28,24 @@ int _printf(const char *format, ...)
 
 		else if (format[t + 1] == 'c')
 		{
-			_putchar(va_arg(args, int);
+			_putchar(va_arg(args, int));
 			t++;
 		}
 		else if (format[t + 1] == 's')
 		{
 			puts_str(va_arg(args, char *));
 			t++;
-			value +=(value - 1); 
+			value += (value - 1); 
 		}
-		else if (format[t + 1] = '%')
+		else if (format[t + 1] == '%')
 		{
 			_putchar('%');
 			t++;
 		}
-		value +=;
+		value += 1;
 		t++;
 	}
+	va_end(args);
+
 	return (value);
 }
