02139a00280860d38644526e9006e7dc843a9453
TKaburu <traceymwendwa@gmail.com>
2023-05-30 10:20:48 -0700

    Updated header file

--- main.h
+++ main.h
@@ -7,6 +7,21 @@
 int _putchar(char c);
 int _printf(const char *format, ...);
 int puts_str(char *string); /*it will help print 's' */
+int (*spec_checker(char format))(va_list);
 
+/**
+ * struct flags - This structure contains the flags specifiers
+ * @plus: This represents the '+' symbol
+ * @space: Thi represents the ' ' symbol
+ * @hash:  This repreent the '#' symbol
+ *
+ */
+
+typedef struct
+{
+	int plus;
+	int space;
+	int hash;
+} get_flag;
 
 #endif
