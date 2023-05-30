e664b22bda6262b36f60a5e42b363848f3ccd6e9
TKaburu <traceymwendwa@gmail.com>
2023-05-30 15:03:23 -0700

    Updated header file

--- main.h
+++ main.h
@@ -6,7 +6,7 @@
 
 int _putchar(char c);
 int print_char (va_list arg);
-
+int bin(va_lit arg);
 int _printf(const char *format, ...);
 int puts_str(char *string); /*it will help print 's' */
 int (*spec_checker(char format))(va_list);
