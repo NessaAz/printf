e1e01847b2ed4885b96cc5065c5fd39edc6ac8e2
TKaburu <traceymwendwa@gmail.com>
2023-06-01 00:22:47 +0300

    These are a group of different specifier functions

---
+++ specifiers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  put_unsigned - print out unsigned ints
+ *  @args: Thee are arguments passed
+ */
+
+int put_unsigned(va_list args)
+{
+	char *string = converter(u, 10, 0)
+	int t = va_list(args, unsigned int);
+
+	return (put_string(string));
+}
+
+
