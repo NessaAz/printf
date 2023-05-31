ea708eb274418ce29ff09bda5315beac0b41420a
TKaburu <traceymwendwa@gmail.com>
2023-06-01 00:07:46 +0300

    This prints out %c

---
+++ put_char.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * put_char - This function prints out a charcter
+ * @args: These re the arguments passed
+ *
+ * Return: 0
+ */
+
+int put_char(va_list args)
+{
+	char t = va_arg(args, int);
+	return (1);
+}
