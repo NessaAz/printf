dd1b06d2eb391db68a33d7b742331c0025b6466f
TKaburu <traceymwendwa@gmail.com>
2023-06-01 00:27:58 +0300

    Updated header file

--- main.h
+++ main.h
@@ -1,17 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+/* C library heder file */
 #include <stdarg.h>
 #include <stdio.h>
+#include <unistd.h>
+#include <stdlib>
 
 int _putchar(char c);
-int print_char (va_list arg);
+int put_char(va_list args);
 int bin(va_list arg);
 int _printf(const char *format, ...);
-void print_string(char *str);
 int print_rot13(va_list arg);
 int put_string(char* strng);
-
+int put_unsigned(va_list args);
+int put_cent(va_list args);
 /**
  * struct flags - This structure contains the flags specifiers
  * @plus: This represents the '+' symbol
