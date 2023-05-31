79a8d06e6a494373aeb2a4a9877214736fb5d567
TKaburu <traceymwendwa@gmail.com>
2023-06-01 00:10:58 +0300

    This prints out %

---
+++ put_cent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * put_cent - This function prints out a "%"
+ * @args: These re the arguments passed
+ *
+ * Return: 0
+ */
+
+int put_cent(va_list args)
+{
+	_putchar('%');
+	return (1);
+}
