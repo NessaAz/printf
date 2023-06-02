1f6cb7d1fe69b77557ca9f360a7152e829646942
Vanessa Azenwa <vanessa.azenwa@gmail.com>
2023-06-02 11:46:15 +0300

    refactor structs

--- main.h
+++ main.h
@@ -12,7 +12,7 @@ int put_char(va_list args);
 int bin(va_list arg);
 int _printf(const char *format, ...);
 int print_rot13(va_list arg);
-int put_string(char* strng);
+int put_string(char *strng);
 int put_unsigned(va_list args);
 int put_cent(va_list args);
 int check_flags(char x, get_flag *f);
@@ -20,14 +20,14 @@ int (*spec_checker(char format))(va_list);
 
 
 /**
- * struct flags - This structure contains the flags specifiers
+ * struct get_flag - This structure contains the flags specifiers
  * @plus: This represents the '+' symbol
  * @space: Thi represents the ' ' symbol
  * @hash:  This repreent the '#' symbol
  *
  */
 
-typedef struct
+typedef struct get_flag
 {
 	int plus;
 	int space;
@@ -36,9 +36,9 @@ typedef struct
 
 
 /**
- * struct specifiers - Specifier and Function Mapping
- * @specifier: specifier character
+ * struct specifier - Specifier and Function Mapping
  * @f: Function pointer to the corresponding print function
+ * @t:  a pointer to a function that takes a va_list parameter and returns an int
 */
 typedef struct specifier
 {
